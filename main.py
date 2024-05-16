9377366887df0e85296ce4d35ec7799ac541eda4
sk3pta <nnnnnmmmmm092@protonmail.com>
2024-05-17 00:25:50 +0300

    lab 5

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello, @username")
+import tensorflow.keras
+
+from tensorflow.keras.utils import Sequential
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from tensorflow.keras.optimizers import RMSprop,Adam
